29a06985bd9e3f25a44444110313474d2eed8573
bahurdeen78 <bahurdeen.emc@gmail.com>
2024-08-31 12:11:27 +0530

    changes

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -1,14 +1,19 @@
 import mongoose  from "mongoose";
-
 const TicketSchema=mongoose.Schema({
+    
     name:{
         type:String,
         required:true
     },
+    number:{
+        type:Number,
+        required:true,
+       
+    },
     email:{
         type:String,
         required:true,
-        unique:true
+       
     },
     place:{
         type:String,
@@ -20,19 +25,19 @@ const TicketSchema=mongoose.Schema({
     },
     plastic:{
         type:Number,
-        required:true, 
+       
     },
     paper:{
         type:Number,
-        required:true,   
+       
     },
     old_untorn_dresses:{
         type:Number,
-        required:true,   
+        
     },
     decarative_materials:{
         type:Number,
-        required:true,   
+       
     },
 
 leather:{
