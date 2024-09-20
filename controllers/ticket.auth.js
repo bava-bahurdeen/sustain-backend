514d11ebeb9b6f1b8d939c64ad9f2d7e52b105cb
bava-bahurdeen <123855151+bava-bahurdeen@users.noreply.github.com>
2024-09-20 15:53:07 +0530

    Update ticket.auth.js

--- controllers/ticket.auth.js
+++ controllers/ticket.auth.js
@@ -27,3 +27,13 @@ export const Ticketpush=async(req,res)=>{
         
     }
   }
+
+    export const TicetbyId=async(req,res)=>{
+      const id= req.params.id
+      try {
+        const ticket= Ticket.findById(id)
+        res.status(200).json(ticket)
+      } catch (error) {
+        res.status(404).json({message:error.message})
+      }
+    }
